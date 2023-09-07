90ef902f85b069cb53e260b079d7cc68231ae2b1
BelloAbdulmujib <belloabdulmujib3@gmail.com>
2023-09-07 05:03:39 -0700

    printc5

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - this  is the stsrt up of my new task
+ *
+ * Return: alwas 0
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
