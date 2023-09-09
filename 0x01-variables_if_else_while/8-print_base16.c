a0f75e7691a6b665e3f593483af83dc11abded87
BelloAbdulmujib <belloabdulmujib3@gmail.com>
2023-09-08 17:04:45 -0700

    8-print_base16.c

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -11,10 +11,10 @@ int main(void)
 	int i;
 	char j;
 
-	for (i = 0 ; i < 10 : i++)
+	for (i = 0 ; i < 10 ; i++)
 		putchar(i = '0');
 	for (j = 'a' ; j <= 'f' ; j++)
 		putchar(j);
-	putcha('\n');
+	putchar('\n');
 	return (0);
 }
