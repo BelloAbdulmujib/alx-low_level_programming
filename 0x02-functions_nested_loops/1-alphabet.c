b5c9261332e681338c071d6415f0fae71d3febac
BelloAbdulmujib <belloabdulmujib3@gmail.com>
2023-09-12 11:10:59 -0700

    1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -2,7 +2,6 @@
 #include <unistd.h>
 
 /**
- * main - function to print task 1
  * print_alphabet - Entry point
  * Return:0
  */
@@ -20,3 +19,4 @@ void print_alphabet(void)
 	_putchar('\n');
 
 }
+
