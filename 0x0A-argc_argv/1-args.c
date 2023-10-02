c9a8e4ebcadbeeac25a143e0eeffbdf6bd866947
BelloAbdulmujib <belloabdulmujib3@gmail.com>
2023-10-02 16:11:28 -0700

    1-args.c

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - program that prints a number
+ * @argc: argument num
+ * @argc: argument vector
+ * Return: 0 always
+ */
+
+int main(int argc, char *argv[])
+{
+	(void) argv;
+
+	printf("%d\n", argc);
+	return (0);
+}
