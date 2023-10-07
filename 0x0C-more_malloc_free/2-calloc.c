db8acb5b46fb4f2622cb84ffdb5059a80d1400c5
BelloAbdulmujib <belloabdulmujib3@gmail.com>
2023-10-06 19:38:41 -0700

    2-calloc.c

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -14,7 +14,7 @@ char *_memset(char *s, char b, unsigned int n)
 
 	for (i = 0; i < n; i++)
 	{
-	s[i] = b;
+		s[i] = b;
 	}
 
 	return (s);
@@ -32,14 +32,14 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 	char *ptr;
 
 	if (nmemb == 0 || size == 0)
-	return (NULL);
+		return (NULL);
 
 	ptr = malloc(size * nmemb);
 
 	if (ptr == NULL)
-	return (NULL);
+		return (NULL);
 
 	_memset(ptr, 0, nmemb * size);
 
-	return (ptr);
+		return (ptr);
 }
