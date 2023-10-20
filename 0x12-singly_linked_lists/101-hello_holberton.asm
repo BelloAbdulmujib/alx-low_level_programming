2cc88d61e1890b0fcaf5f803e48b05438077166e
AbdulmujibBello <belloabdulmujib3@gmail.com>
2023-10-19 20:53:45 -0700

    2-add_node.c

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,18 +1,10 @@
-section .data
-    hello db 'Hello, Holberton', 10, 0 ; 10 is the ASCII code for a new line (LF)
-
-section .text
-    global main
-
+global    main
+          extern    printf
 main:
-    push rbp
-    mov rbp, rsp
-
-    lea rdi, [hello]
-    xor eax, eax
-    call printf
-
-    mov rsp, rbp
-    pop rbp
-    ret
+	  mov   edi, format
+	  xor   eax, eax
+	  call  printf
+	  mov 	eax, 0
+	  ret
+format: db `Hello, Holberton\n`,0
 
--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -1,4 +1,6 @@
-#include "list.h"
+#include <stdlib.h>
+#include <string.h>
+#include "lists.h"
 
 /**
  * add_node - adds a new node at the beginning of a list_t list
@@ -9,26 +11,26 @@
  */
 list_t *add_node(list_t **head, const char *str)
 {
-    list_t *new_node;
+	list_t *new_node;
 
-    if (head == NULL || str == NULL)
-        return (NULL);
+	if (head == NULL || str == NULL)
+	return (NULL);
 
-    new_node = malloc(sizeof(list_t));
-    if (new_node == NULL)
-        return (NULL);
+	new_node = malloc(sizeof(list_t));
+	if (new_node == NULL)
+	return (NULL);
 
-    new_node->str = strdup(str);
-    if (new_node->str == NULL)
-    {
-        free(new_node);
-        return (NULL);
-    }
+	new_node->str = strdup(str);
+	if (new_node->str == NULL)
+	{
+	free(new_node);
+	return (NULL);
+	}
 
-    new_node->len = _strlen(new_node->str);
-    new_node->next = *head;
-    *head = new_node;
+	new_node->len = strlen(new_node->str);
+	new_node->next = *head;
+	*head = new_node;
 
-    return (new_node);
+	return (new_node);
 }
 
