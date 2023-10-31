b56d4ce5ec99c561fa5ba482f2efe814ac67b1ef
AbdulmujibBello <belloabdulmujib3@gmail.com>
2023-10-30 17:19:49 -0700

    0x15-file_io

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -7,7 +7,7 @@
 #define BUFFER_SIZE 1024
 
 /**
- * main - copies the content of a file
+ * main - copies the content of a file to another file
  * @argc: number of arguments passed to the program
  * @argv: array of arguments passed to the program
  *
@@ -29,13 +29,13 @@ int main(int argc, char **argv)
 
 	fd_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
 	if (fd_to == -1)
-		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]), exit(99);
+	dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]), exit(99);
 
 	while ((nread = read(fd_from, buffer, BUFFER_SIZE)) > 0)
 	{
 		ret = write(fd_to, buffer, nread);
 		if (ret == -1)
-			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]), exit(99);
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]), exit(99);
 	}
 
 	if (nread == -1)
