989e994e4ca88aa2d1d5e2fc66c6ffce2ce0e6e7
AbdulmujibBello <belloabdulmujib3@gmail.com>
2023-10-30 17:02:43 -0700

    0x15-file_io

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -1,11 +1,17 @@
 #ifndef MAIN_H
 #define MAIN_H
+#include <stdio.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <errno.h>
 
 int _putchar(char c);
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
 int main(int argc, char **argv);
-void print_elf_header(Elf64_Ehdr *header);
+
 
 #endif
