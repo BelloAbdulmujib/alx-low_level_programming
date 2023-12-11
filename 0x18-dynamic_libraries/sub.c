4b947c33bec03a11fc330df68571ac0a4635899a
AbdulmujibBello <belloabdulmujib3@gmail.com>
2023-12-11 15:53:20 -0800

    0x18-dynamic_libraries

--- 0x18-dynamic_libraries/sub.c
+++ 0x18-dynamic_libraries/sub.c
@@ -1,13 +1,13 @@
 #include <stdlib.h>
 #include <stdio.h>
 /**
- * substraction - function that substract two integers
+ * sub - function that substract two integers
  * @a:  first parameter
  * @b: second parameter
  * Return: final result
  */
 
-int substract(int a, int b)
+int sub(int a, int b)
 {
 	return (a - b);
 }
